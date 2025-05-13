f3596d8bed8a340f81fafa7ccbaf8c74d91f98dd
Itai Sitnik <sitnik@campus.technion.ac.il>
2025-05-13 21:45:31 +0300

    Made BT demo work

--- flutter_app/Demo Bluetooth App/lib/main.dart
+++ flutter_app/Demo Bluetooth App/lib/main.dart
@@ -1,122 +1,318 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bluetooth Flutter App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  BluetoothConnection? connection;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _connect,
+          child: Text('Find BT Device'),
+        ),
+      ),
+    );
+  }
+
+  void _connect() async {
+    BluetoothDevice? selectedDevice = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => DeviceListPage()),
+    );
+
+    if (selectedDevice != null) {
+      try {
+        connection =
+            await BluetoothConnection.toAddress(selectedDevice.address);
+        // connection = await BluetoothConnection.toAddress('B0:A7:32:DB:0C:9A'); //try using this if you don't want to select the device every time
+        // incoming messages will be handled by _onDataReceived
+
+        print('Connected to ${selectedDevice.name}');
+
+        if (connection != null) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => CheckScreen(connection: connection!)),
+          );
+        }
+      } catch (e) {
+        print('Error connecting: $e');
+      }
+    }
+  }
+}
+
+class DeviceListPage extends StatelessWidget {
+  Future<List<BluetoothDevice>> _getBondedDevices(BuildContext context) async {
+    List<BluetoothDevice> devices = [];
+    try {
+      devices = await FlutterBluetoothSerial.instance.getBondedDevices();
+    } catch (e) {
+      print('Error getting bonded devices: $e');
+    }
+    return devices;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Device'),
+      ),
+      body: FutureBuilder<List<BluetoothDevice>>(
+        future: _getBondedDevices(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No bonded devices found.'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                BluetoothDevice device = snapshot.data![index];
+                return ListTile(
+                  title: Text(device.name ?? 'Unknown Name'),
+                  subtitle: Text(device.address ?? 'Unknown Address'),
+                  onTap: () {
+                    Navigator.pop(context, device);
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CheckScreen extends StatefulWidget {
+  final BluetoothConnection connection;
+  // final String userName;
+
+  CheckScreen({required this.connection});
+
+  @override
+  _CheckScreenState createState() => _CheckScreenState();
+}
+
+class _CheckScreenState extends State<CheckScreen> {
+  int? elapsedTime; // New state variable for elapsed time
+  String stage = "0";
+  String _messageBuffer = '';
+  List<String> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _startListening();
+  }
+
+  void _startListening() {
+    widget.connection.input!.listen((Uint8List data) {
+      _onDataReceived(data);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('bluetooth classic test'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () => _showSettings(context),
+              child: Text('Send settings to ESP32'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              //this button does nothing
+              onPressed: () {},
+              child: Text('Incoming Messages:'),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return Center(
+                    child:
+                        Text(messages[index])); // Wrap Text widget with Center
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _showSettings(BuildContext context) {
+    bool isSlowMode = false;
+    String? selectedSoundSet = 'sound_set_1'; // Default value
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Settings'),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SwitchListTile(
+                    title: Text('Mode'),
+                    value: isSlowMode,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isSlowMode = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Choose Sound',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Sound Set 1'),
+                    leading: Radio(
+                      value: 'S1',
+                      groupValue: selectedSoundSet,
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedSoundSet = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Sound Set 2'),
+                    leading: Radio(
+                      value: 'S2',
+                      groupValue: selectedSoundSet,
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedSoundSet = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Sound Set 3'),
+                    leading: Radio(
+                      value: 'S3',
+                      groupValue: selectedSoundSet,
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedSoundSet = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                ],
+              );
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                _sendSettings(isSlowMode, selectedSoundSet);
+                Navigator.of(context).pop(); // Close the settings dialog
+              },
+              child: Text('Save Settings'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _sendSettings(bool isSlowMode, String? selectedSoundSet) {
+    String str = '';
+
+    if (!isSlowMode) {
+      str += 'M1';
+    } else {
+      str += 'M2';
+    }
+    str += ',';
+    str += '$selectedSoundSet';
+    str += '#';
+    if (widget.connection.isConnected) {
+      Uint8List data = Uint8List.fromList(utf8.encode(str));
+      widget.connection.output.add(data);
+      widget.connection.output.allSent.then((_) {
+        print('sending: ' + str);
+      });
+    } else {
+      print('Not connected');
+    }
+  }
+
+  void _onDataReceived(Uint8List data) {
+    Uint8List buffer = Uint8List(data.length);
+    int bufferIndex = buffer.length;
+    for (int i = 0; i < data.length; i++) {
+      buffer[i] = data[i];
+    }
+
+    String dataString = String.fromCharCodes(buffer);
+
+    int index = buffer.indexOf(35); // 35 is the # character
+    if (~index != 0) {
+      messages.add(_messageBuffer + dataString.substring(0, index));
+      if (messages.length > 5) {
+        messages.removeRange(
+            0, messages.length - 5); //keep only the last 5 incoming messages
+      }
+      _messageBuffer =
+          dataString.substring(index + 1); // Add +1 to skip the # character
+      print("messages: " + messages.toString());
+      setState(() {
+        // Trigger an update of the scaffold to show the new messages
+      });
+    } else {
+      _messageBuffer = _messageBuffer + dataString;
+    }
   }
 }
